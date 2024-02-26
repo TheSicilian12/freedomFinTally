f5d0e8dd355d5a72b8384de991b4529c3325d5c3
TheSicilian12 <mguidera314@gmail.com>
2024-02-26 11:14:41 -0500

    removing variables

--- script.js
+++ script.js
@@ -1,51 +1,4 @@
-// $(document).ready(function(){
-//     $(".hello").on('click', function(){
-//         $(this).addClass('blue');
-//     });
-
-
-//     $("#myForm").on('submit', function(event){
-//         event.preventDefault();
-
-//         let inputValue = $('.input').val();
-//         $('.content').append('<div>' + inputValue + '</div>')
-//     })
-
-//     $("#clearButton").on('click', function(event){
-//         event.preventDefault();
-
-//         $('.content').empty()
-//     })
-
-// });
-
-// $(document).ready(function () {
-//     $(".hello").on('click', function () {
-//         $(this).addClass('blue');
-//     });
-
-//     // Event delegation for form submission
-//     $(document).on('submit', '.myForm', function (event) {
-//             event.preventDefault();
-//             // let inputValue = $('.input').val();
-//         //    $('.content').append('<div>' + inputValue + '</div>');
-//         let inputValue = $(this).find('.input').val();
-//         $(this).closest('.content').append('<div>' + inputValue + '</div>');
-//     });
-
-//     // Event delegation for clear button click
-//     $(document).on('click', '#clearButton', function (event) {
-//         event.preventDefault();
-//         $('.content').empty();
-//     });
-// });
-
 $(document).ready(function () {
-    let totalVal = 0;
-    let cashSubTotal = 0;
-    let checkSubTotal = 0;
-    let checkCounter = 0;
-
     // Event delegation for form submission
     $(document).on('submit', '#checkForm, #cashForm', function (event) {
         event.preventDefault();
@@ -61,26 +14,10 @@ $(document).ready(function () {
             let newSubTotal = currentSubTotal + inputValue;
             let newTotal = currentTotal + inputValue;
 
-            // totalVal += inputValue;
-            // console.log(formId, formId === "cashForm")
-            // if (formId === "cashForm") {
-            //     cashSubTotal += inputValue;
-            // }
-            // else {
-            //     checkSubTotal += inputValue;
-            //     checkCounter++;
-            // }
-
             $(this).closest('div').find('.content').append('<div class="check-info-container"><div class= "check-info"> ' + '$' + inputValue.toLocaleString('en', { useGrouping: true }) + '</div> <button id="clearCheck" class="button-clear">X</button></div > '); // Append to the closest content section relative to the form
             $(this).find('#subTotal').text(newSubTotal.toLocaleString('en', { useGrouping: true }));
             $('#total').text(newTotal.toLocaleString('en', { useGrouping: true }));
             $(this).closest('form').find('.input').val(''); // Clear the input field
-
-
-            $("#report-total").text(newTotal)
-            $("#report-cash-subTotal").text(cashSubTotal);
-            $("#report-check-subTotal").text(checkSubTotal);
-            $("#report-check-tally").text(checkCounter);
         }
     });
 
