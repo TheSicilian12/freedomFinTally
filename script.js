e85353cd8129f00386b6cdb7a1c20fbbe98bbdba
TheSicilian12 <mguidera314@gmail.com>
2024-03-05 12:33:04 -0500

    check form fix

--- script.js
+++ script.js
@@ -29,6 +29,8 @@ $(document).ready(function () {
         $('#lima-cash-subTotal').text((limaCashVal + limaCashSubTotal).toLocaleString('en', { useGrouping: true }));
         $('#lima-check-subTotal').text((limaCheckVal + limaCheckSubTotal).toLocaleString('en', { useGrouping: true }));
 
+        let newTotal = $('#total').text();
+        $('#report-total').text(newTotal);
 
         if (ottawaCheckVal) {
             $('#ottawa-add-checks').append('<div class="check-info-container"><div class= "check-info"> ' + '$' + ottawaCheckVal.toLocaleString('en', { useGrouping: true }) + '</div> <button class="button-clear ottawa-clearCheck">X</button></div > ');
@@ -62,24 +64,26 @@ $(document).ready(function () {
         event.preventDefault();
 
         let totalValue = Number($('#total').text().split(',').join(''));
-        let ottawaCheckSubTotal = Number($('#lima-cash-subTotal').text().split(',').join(''));
+        let ottawaCheckSubTotal = Number($('#ottawa-check-subTotal').text().split(',').join(''));
 
+        $('#total').text((totalValue - ottawaCheckSubTotal).toLocaleString('en', { useGrouping: true }));
         $('#ottawa-check-subTotal').text(0);
         $('#ottawa-add-checks').empty();
-        $('#total').text((totalValue - ottawaCheckSubTotal).toLocaleString('en', { useGrouping: true }));
     });
 
     $(document).on('click', '#lima-check-clearButton', function (event) {
         event.preventDefault();
 
         let totalValue = Number($('#total').text().split(',').join(''));
-        let limaCheckSubTotal = Number($('#lima-cash-subTotal').text().split(',').join(''));
+        let limaCheckSubTotal = Number($('#lima-check-subTotal').text().split(',').join(''));
 
-        $('#lima-check-subTotal').text(0);
-        $('#lima-add-checks').empty();
         $('#total').text((totalValue - limaCheckSubTotal).toLocaleString('en', { useGrouping: true }));
+        $('#lima-add-checks').empty();
+        $('#lima-check-subTotal').text(0);
     });
 
+
+    // individual checks
     $(document).on('click', '.lima-clearCheck', function (event) {
         event.preventDefault();
 
@@ -111,74 +115,4 @@ $(document).ready(function () {
 
         parent.remove();
     });
-
-
 });
-
-
-
-
-
-//     // Event delegation for form submission
-//     $(document).on('submit', '#checkForm, #cashForm', function (event) {
-//         event.preventDefault();
-
-//         let inputValue = Number($(this).find('.input').val());
-//         let formId = $(this).attr('id');
-//         let formSource = $(this).data('source');
-
-//         if (inputValue > 0) {
-//             let currentSubTotal = Number($(this).find('#subTotal').text().split(',').join(''))
-//             let currentTotal = Number($('#total').text().split(',').join(''))
-
-//             let newSubTotal = currentSubTotal + inputValue;
-//             let newTotal = currentTotal + inputValue;
-
-//             $(this).closest('div').find('.content').append('<div class="check-info-container"><div class= "check-info"> ' + '$' + inputValue.toLocaleString('en', { useGrouping: true }) + '</div> <button id="clearCheck" class="button-clear">X</button></div > '); // Append to the closest content section relative to the form
-//             $(this).find('#subTotal').text(newSubTotal.toLocaleString('en', { useGrouping: true }));
-//             $('#total').text(newTotal.toLocaleString('en', { useGrouping: true }));
-//             $(this).closest('form').find('.input').val(''); // Clear the input field
-//         }
-//     });
-
-//     // Event delegation for clear button click
-//     $(document).on('click', '#clearButton', function (event) {
-//         event.preventDefault();
-//         $(this).closest('form').find('.content').empty();
-//         $(this).closest('form').find('.content').append('<div class="checks-text">Checks</div>');
-//         $(this).closest('form').find('.input').val(''); // Clear the input field
-
-//         let currentTotal = Number($('#total').text().split(',').join(''));
-//         let subTotal = Number($(this).closest('form').find('#subTotal').text().split(',').join(''));
-
-//         currentTotal -= subTotal;
-
-//         let clearType = $(this).attr('id');
-
-//         // let newTotal = currentTotal - subTotal;
-//         $('#total').text(currentTotal.toLocaleString('en', { useGrouping: true }))
-//         $(this).closest('form').find('#subTotal').text('0');
-//     });
-
-//     $(document).on('click', '#clearCheck', function (event) {
-//         event.preventDefault();
-//         console.log("clearCheck")
-//         let formId = $(this).attr('id');
-//         console.log(formId)
-
-//         let parent = $(this).parent();
-//         let checkDiv = parent.find('.check-info');
-//         let checkVal = parseFloat(checkDiv.text().replace('$', '').replace(/,/g, ''));
-
-//         let currentTotal = Number($('#total').text().split(',').join(''));
-//         let subTotal = Number($(this).closest('form').find('#subTotal').text().split(',').join(''));
-
-//         currentTotal -= 0.5 * checkVal;
-//         subTotal -= checkVal;
-
-//         $('#total').text(currentTotal.toLocaleString('en', { useGrouping: true }))
-//         $(this).closest('form').find('#subTotal').text(subTotal);
-
-//         parent.remove();
-//     })
-// });
